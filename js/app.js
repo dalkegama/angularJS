3bdc02b020c98e63a44ce42af5350252524e448d
dalkegama <dalkegama@dcthomson.co.uk>
2015-01-29 17:11:37 +0000

    updates

--- js/app.js
+++ js/app.js
@@ -2,47 +2,28 @@
  * Created by dalkegama on 21/01/2015.
  */
 
-'use strict';
+
 
 var myApp = angular.module('myApp',[
-    'myApp.filter'
+    'myApp.filter',
 ]);
 
-myApp.controller('MyController',['$scope','$http', function ($scope,$http) {
-    $http.get('http://pipes.yahoo.com/pipes/pipe.run?_id=f53de95ad9cfa91e4de371f75f16ab75&_render=json').success(function(data){
-        $scope.newsItems = data.value.items;
-        console.log(data.value.items);
-    });
-    //$scope.colorList = [
-    //    {
-    //        "colorName": "red",
-    //        "hexValue": "#f00"
-    //    },
-    //    {
-    //        "colorName": "green",
-    //        "hexValue": "#0f0"
-    //    },
-    //    {
-    //        "colorName": "blue",
-    //        "hexValue": "#00f"
-    //    },
-    //    {
-    //        "colorName": "cyan",
-    //        "hexValue": "#0ff"
-    //    },
-    //    {
-    //        "colorName": "magenta",
-    //        "hexValue": "#f0f"
-    //    },
-    //    {
-    //        "colorName": "yellow",
-    //        "hexValue": "#ff0"
-    //    },
-    //    {
-    //        "colorName": "black",
-    //        "hexValue": "#000"
-    //    }
-    //];
+myApp.controller('NewsController',['$scope','$http','$interval',
+    function ($scope,$http,$interval) {
+        $scope.newsItems = [];
+
+        $http.jsonp('http://pipes.yahoo.com/pipes/pipe.run?_id=f53de95ad9cfa91e4de371f75f16ab75&_render=json&_callback=JSON_CALLBACK')
+        .success(function(data){
+                if(data.value.items){
+                    $scope.newsItems
+                }
+            $scope.newsItems = data.value.items;
+        });
+
+        $interval(function(news){
+            $scope.newsItems.push(news);
+            console.log($scope.newsItems);
+        },5000);
 }]);
 
 angular.module('myApp.filter',[])
